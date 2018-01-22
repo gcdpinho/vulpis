e73861f4110455e8f291bc36923c9512cb3d2ac5
Gustavo Pinho <gustavo.pinho@gavb.com.br>
2018-01-22 15:50:02 -0200

    orçamentos

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,13 +1,13 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
   "use strict";
 
   //Contact
-  $('form.contactForm').submit(function() {
+  $('form.contactForm').submit(function () {
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
+    f.children('input').each(function () { // run all inputs
 
       var i = $(this); // current input
       var rule = i.attr('data-rule');
@@ -24,7 +24,7 @@ jQuery(document).ready(function($) {
 
         switch (rule) {
           case 'required':
-            if (i.val() === '') {
+            if (i.val() === '' && $('.orcamento').css('display') != "none") {
               ferror = ierror = true;
             }
             break;
@@ -57,7 +57,7 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    f.children('textarea').each(function() { // run all inputs
+    f.children('textarea').each(function () { // run all inputs
 
       var i = $(this); // current input
       var rule = i.attr('data-rule');
@@ -88,27 +88,113 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
+
+    f.children('select').each(function () { // run all select
+
+      var i = $(this); // current input
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+        var pos = rule.indexOf(':', 0);
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+        switch (rule) {
+          case 'required':
+            if (i.val() === null && $('.orcamento').css('display') != "none") {
+              ferror = ierror = true;
+            }
+            break;
+        }
+        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+      }
+    });
     if (ferror) return false;
-    else var str = $(this).serialize();
+    else {
+      if ($(".orcamento").css("display") == "none")
+        var data = {
+          type: "contato",
+          name: $("#name").val(),
+          email: $("#email").val(),
+          companyName: $("#companyName").val(),
+          message: $("#message").val()
+        }
+      else
+        var data = {
+          type: "orcamento",
+          name: $("#name").val(),
+          email: $("#email").val(),
+          companyName: $("#companyName").val(),
+          service: getService($("#service").val()),
+          date: $("#date").val(),
+          site: $("input[name='site']:checked").val(),
+          logo: $("input[name='logo']:checked").val(),
+          message: $("#message").val()
+        }
+    }
+
     $.ajax({
       type: "POST",
-      url: "contactform/contactform.php",
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
+      url: "contactform.php",
+      cache: false,
+      data: data,
+      success: function () {
+        $("#sendmessage").addClass("show");
+        $("#errormessage").removeClass("show fadeIn animated");
+        $('.contactForm').find("input, textarea").val("");
+        setTimeout(function () {
+          $("#sendmessage").removeClass("fadeIn animated");
+          $("#sendmessage").addClass("fadeOut animated");
+          setTimeout(function () {
+            $('#sendmessage').removeClass("show");
+            $(".orcamento").css("display", "none");
+            $("#message").attr('placeholder', 'Mensagem')
+            $("#sendmessage").removeClass("fadeOut animated");
+          }, 1000);
+        }, 3000);
 
+      },
+      error: function () {
+        $("#sendmessage").removeClass("show");
+        $("#errormessage").addClass("show fadeIn animated");
+        setTimeout(function () {
+          $("#errormessage").removeClass("fadeIn animated");
+          $("#errormessage").addClass("fadeOut animated");
+          setTimeout(function () {
+            $('#errormessage').removeClass("show");
+            $("#errormessage").removeClass("fadeOut animated");
+          }, 1000);
+        }, 3000);
       }
     });
     return false;
   });
+
+  function getService(number) {
+    switch (number) {
+      case 1:
+        return "Websites";
+      case 2:
+        return "Aplicativos Móveis";
+      case 3:
+        return "Sistemas Empresariais";
+      case 4:
+        return "Manutenção de Sites";
+      case 5:
+        return "Branding Digital";
+      case 6:
+        return "SEO";
+      case 7:
+        return "E-Commerce";
+      case 8:
+        return "E-mail Marketing";
+      case 9:
+        return "Foto Still";
+    }
+  }
 
 });
---
+++ contactform/contactform.php
@@ -0,0 +1,25 @@
+<?php
+$type = strip_tags(htmlspecialchars($_POST['type']));
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$companyName = strip_tags(htmlspecialchars($_POST['companyName']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+$email_orcamento = "";
+if ($type == "orcamento"){
+    $service = strip_tags(htmlspecialchars($_POST['service']));
+    $date = strip_tags(htmlspecialchars($_POST['date']));
+    $site = strip_tags(htmlspecialchars($_POST['site']));
+    $logo = strip_tags(htmlspecialchars($_POST['logo']));
+
+    $email_orcamento = "Serviço: $service\n\nPrazo: $date\n\nPossui site? $site\n\nPossui logo? $logo";
+}
+	
+$to = 'gcdpinho@gmail.com';
+$email_subject = "Mensagem do site:  $name";
+$email_body = "Você recebeu uma nova mensagem do seu site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email\n\nEmpresa: $companyName\n\nMensagem:\n$message";
+$headers = "De: noreply@vulpis.com.br\n";
+$headers .= "Responder a: $email";	
+mail($to,$email,$email_body+$email_orcamento,$headers);
+return true;			
+?>
